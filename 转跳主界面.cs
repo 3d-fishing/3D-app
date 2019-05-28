bdf4d063fb0363a002cf8841a501b5ffac084b3e
hongdi878787 <48280113+hongdi878787@users.noreply.github.com>
2019-05-28 10:20:30 +0800

    Add files via upload

---
+++ 转跳主界面.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class 转跳主界面 : MonoBehaviour {
+
+	public Button mButton;
+	public GameObject caidan;
+
+	// Use this for initialization
+	void Start () {
+		//Gets ButtonMount
+		Button btnMount = mButton.GetComponent<Button>();
+		//add a listener to ButtonMount, executing TaskOnClick() when click ButtonMount
+		btnMount.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		//Loading Scene1
+		caidan.SetActive (true);
+		UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+	}
+
+}
---
+++ 长按.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class 长按 : MonoBehaviour 
+{
+	public float min;
+	public float max;
+	public GameObject yugan;
+	private bool IsStart = false;
+	private float LastTime = 0;
+
+	public GameObject btnObj;
+	public GameObject caidan;
+	public GameObject caidan2;
+	public Sprite   expan;
+	public Sprite  back;
+	Button btn;
+	bool isshow=false ;
+
+	void Start () {
+
+		caidan.SetActive (isshow);
+		caidan2.SetActive (isshow);
+		btn = btnObj.GetComponent<Button>();
+		btn.onClick.AddListener(delegate ()
+			{
+				isshow=!isshow;
+				caidan.SetActive (isshow);
+				caidan2.SetActive (isshow);
+				if (isshow)
+				{
+					btn.GetComponent<Image>().sprite=expan ;
+				}
+				else {
+					btn.GetComponent<Image>().sprite=back;
+				}
+			});
+	}
+
+	void Update () {
+		
+		if (IsStart && min > 0 && LastTime > 0 && Time.time - LastTime > min) {
+			yugan.transform.localEulerAngles = new Vector3 (0.0f, 0.0f, 0.0f);
+			yugan.transform.localPosition = new Vector3(3.3f,0.0f,5f);
+			IsStart = false;
+			LastTime = 0;
+		} 
+	}
+
+	public void LongPress(bool bStart)
+	{
+		IsStart = bStart;
+		if(IsStart)
+		{
+			LastTime = Time.time;
+		}
+		else if(LastTime != 0)
+		{
+			LastTime = 0;
+		}    
+	}
+
+}
+	
