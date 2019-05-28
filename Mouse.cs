ebc8909d8168177a8ac574d09087c63742acf9cd
hongdi878787 <48280113+hongdi878787@users.noreply.github.com>
2019-05-28 10:22:16 +0800

    Add files via upload

---
+++ Mouse.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using Game_Manager;
+
+namespace Game_Manager {
+
+	public class Game_Scene_Manager : System.Object {
+		private static Game_Scene_Manager _instance;
+		private static Mouse_Image _Mouse;
+		private int IsHair = 0; // 是否头部有装备
+		private int IsWeapon = 0; // 是否有武器装备
+		private int IsFoot = 0; // 是否有脚部装备
+
+		public static Game_Scene_Manager GetInstance() {
+			if (_instance == null) {
+				_instance = new Game_Scene_Manager();
+			}
+			return _instance;
+		}
+
+		public void SetMouse(Mouse_Image _mouse) {
+			if (_Mouse == null) {
+				_Mouse = _mouse;
+			}
+		}
+
+		public Mouse_Image GetMouse() {
+			return _Mouse;
+		}
+
+		public void GenAll() {
+			IsFoot = 1;
+			IsHair = 1;
+			IsWeapon = 1;
+		}
+
+		public int GetHair() { return IsHair; }
+		public int GetWeapon() { return IsWeapon; }
+		public int GetFoot() { return IsFoot; }
+
+		public void SetHair(int a) { IsHair = a; }
+		public void SetWeapon(int a) { IsWeapon = a; }
+		public void SetFoot(int a) { IsFoot = a; }
+	}
+
+}
+
+public class Mouse : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Mouse_Image.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using Game_Manager;
+using UnityEngine.UI;
+
+public class Mouse_Image : MonoBehaviour {
+
+	private Game_Scene_Manager gsm;
+	private Image mouse_image;
+	private int mouse_type = 0;
+	public Sprite none;
+	public Sprite hair; // 保存相应的装备
+	public Sprite weapon; // 保存相应的装备
+	public Sprite foot; // 保存相应的装备
+	public Color None;
+	public Color NotNone;
+	public Camera cam;
+
+	void Awake() {
+		gsm = Game_Scene_Manager.GetInstance();
+		gsm.SetMouse(this);
+		mouse_image = GetComponent<Image>();
+	}
+
+	public int GetMouseType() {
+		return mouse_type;
+	}
+
+	public void SetMouseType(int Mouse_type) {
+		mouse_type = Mouse_type;
+	}
+
+	void Update () {
+		if (mouse_type == 0) // 每一帧进行更新，检查鼠标上是否需要有装备，根据mousetype更新，如果mousetype为0则装备无
+		{
+			mouse_image.sprite = none;
+			mouse_image.color = None;
+		}
+		else
+		{ // mousetype不为零，根据mousetype加上相应装备
+			//Debug.Log("I am mouse image");
+			//Debug.Log(mouse_type);
+
+			mouse_image.color = new Color(1F, 1F, 1F, 1F);
+			//Debug.Log(mouse_image.color);
+			if (mouse_type == 1) mouse_image.sprite = hair;
+			else if (mouse_type == 2) mouse_image.sprite = weapon;
+			else if (mouse_type == 3) mouse_image.sprite = foot;
+		}
+		transform.position = new Vector3 (Input.mousePosition.x-600, Input.mousePosition.y-230, 0);
+	}
+}
